b59d4a877f2c6bc015d13c811fe34bcf5890f0cd
Alex Francisco <alexfsdn@gmail.com>
2022-11-02 10:03:23 -0300

    desenv para pegar parâmetros de arquivo de configuração

--- src/main/scala/ingestion/base/config/Config.scala
+++ src/main/scala/ingestion/base/config/Config.scala
@@ -18,4 +18,34 @@ object Config {
 
   def getFileName: String = config.getString("file_name")
 
+  def getDataBase: String = config.getString("database")
+
+  def getJobName: String = config.getString("job_name")
+
+  def getTable: String = config.getString("table")
+
+  def getPartitionName: String = config.getString("col_partition_name")
+
+  def getIngestionName: String = config.getString("col_ingestion_name")
+
+  def getFormat: String = config.getString("format")
+
+  def getMap: String = config.getString("map_list")
+
+  def getDataBaseControl: String = config.getString("database_control")
+
+  def getTableControl: String = config.getString("table_control")
+
+  def getColumnOrder: String = config.getString("col_order")
+
+  def getControlColumnOrder: String = config.getString("col_order_table_control")
+
+  def getUrlHdfs: String = config.getString("hdfs_url")
+
+  def getInputPath: String = config.getString("input_path")
+
+  def getArchiving: String = config.getString("archiving_path")
+
+  def getArchivingError: String = config.getString("archiving_error_path")
+
 }
--- src/test/scala/ingestion/base/config/ConfigTest.scala
+++ src/test/scala/ingestion/base/config/ConfigTest.scala
@@ -2,8 +2,18 @@ package ingestion.base.config
 
 import org.junit.Test
 
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
+
 class ConfigTest {
 
+  @Test def testMap(): Unit = {
+    implicit val formats = org.json4s.DefaultFormats
+
+    val result: Map[String, String] = parse(Config.getMap).extract[Map[String, String]]
+
+    result.foreach(a => println(a))
+  }
 
   @Test def test(): Unit = {
     println("config...")
@@ -11,5 +21,82 @@ class ConfigTest {
     println(".........")
 
     println("file_name=" + Config.getFileName)
+    println(".........")
+    println(".........")
+
+    println("database=" + Config.getDataBase)
+
+    println(".........")
+    println(".........")
+
+    println("job_name=" + Config.getJobName)
+
+    println(".........")
+    println(".........")
+
+    println("table=" + Config.getTable)
+
+    println(".........")
+    println(".........")
+
+    println("col_partition_name=" + Config.getPartitionName)
+
+
+    println(".........")
+    println(".........")
+
+    println("col_ingestion_name=" + Config.getIngestionName)
+
+
+    println(".........")
+    println(".........")
+
+    println("format=" + Config.getFormat)
+
+    println(".........")
+    println(".........")
+
+    println("database_control=" + Config.getDataBaseControl)
+
+
+    println(".........")
+    println(".........")
+
+    println("table_control=" + Config.getTableControl)
+
+
+    println(".........")
+    println(".........")
+
+    println("col_order=" + Config.getColumnOrder)
+
+    println(".........")
+    println(".........")
+
+    println("col_order_table_control=" + Config.getTableControl)
+
+
+    println(".........")
+    println(".........")
+
+    println("hdfs_url=" + Config.getUrlHdfs)
+
+
+    println(".........")
+    println(".........")
+
+    println("input_path=" + Config.getInputPath)
+
+
+    println(".........")
+    println(".........")
+
+    println("archiving_path=" + Config.getArchiving)
+
+
+    println(".........")
+    println(".........")
+
+    println("archiving_error_path=" + Config.getArchivingError)
   }
 }
