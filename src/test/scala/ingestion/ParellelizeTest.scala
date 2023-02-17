5399c53757209faaecf7d56bea9c97051bb1604a
Alex Francisco <alexfsdn@gmail.com>
2023-02-17 09:39:15 -0300

    estudo de repartition e coalesce

--- src/test/scala/ingestion/ParellelizeTest.scala
+++ src/test/scala/ingestion/ParellelizeTest.scala
@@ -1,6 +1,7 @@
 package ingestion
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{Dataset, SaveMode, SparkSession}
 import org.junit.Test
 
 class ParellelizeTest {
@@ -12,14 +13,39 @@ class ParellelizeTest {
       .appName("SparkByExamples.com")
       .getOrCreate()
 
-    val rdd = spark.sparkContext.parallelize(Range(0, 20))
+    val rdd: RDD[Int] = spark.sparkContext.parallelize(Range(0, 20))
     println("From local[5]" + rdd.partitions.size)
 
-    val rdd1 = spark.sparkContext.parallelize(Range(0, 20), 6)
+    val rdd1: RDD[Int] = spark.sparkContext.parallelize(Range(0, 20), 6)
     println("parallelize : " + rdd1.partitions.size)
 
-    val rddFromFile = spark.sparkContext.textFile("src/test/resources/test.txt", 10)
+    val rddFromFile: RDD[String] = spark.sparkContext.textFile("src/test/resources/test.txt", 10)
     println("TextFile : " + rddFromFile.partitions.size)
+
+    //rdd1.saveAsTextFile("/tmp/partition")
+
+    val rdd2: RDD[Int] = rdd1.repartition(4)
+    println("Repartition size : " + rdd2.partitions.size)
+    //rdd2.saveAsTextFile("/tmp/re-partition")
+
+    val rdd3: RDD[Int] = rdd1.coalesce(4)
+    println("Repartition size : " + rdd3.partitions.size)
+    rdd3.saveAsTextFile("/tmp/coalesce")
+
+
+  }
+
+  @Test
+  def testDataFrame(): Unit = {
+    val spark: SparkSession = SparkSession.builder()
+      .master("local[5]")
+      .appName("SparkByExamples.com")
+      .getOrCreate()
+
+    val df: Dataset[java.lang.Long] = spark.range(0, 20)
+    println(df.rdd.partitions.length)
+
+    df.write.mode(SaveMode.Overwrite) csv ("partition.csv")
   }
 
 }
