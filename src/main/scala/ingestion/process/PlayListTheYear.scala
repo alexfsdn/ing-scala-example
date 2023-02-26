4818e4396c2f9086adeb190c270811c7e341033e
Alex Francisco <alexfsdn@gmail.com>
2023-02-26 01:42:35 -0300

    atualizando PlayListTheYear e PlayListTheYearTest

--- src/main/scala/ingestion/process/PlayListTheYear.scala
+++ src/main/scala/ingestion/process/PlayListTheYear.scala
@@ -3,7 +3,7 @@ package ingestion.process
 import ingestion.base.dados.ISpark
 import ingestion.base.enums.StatusEnums
 import ingestion.util.{TodayUtils, ValidParamUtils}
-import org.apache.spark.sql.functions.{col, lit, substring, sum}
+import org.apache.spark.sql.functions.{avg, col, collect_set, count, countDistinct, current_date, date_format, first, last, lit, max, mean, min, substring, sum, sumDistinct, when}
 
 class PlayListTheYear(iSpark: ISpark, today: TodayUtils, validParamUtils: ValidParamUtils) {
   private var TABLE_NAME_INGESTION = ""
@@ -58,30 +58,48 @@ class PlayListTheYear(iSpark: ISpark, today: TodayUtils, validParamUtils: ValidP
       val dfPlayList = iSpark.get(s"select * from $PLAY_LIST_TABLE")
 
       println(s"building dfFinal ...")
-      val dfFinal = dfUser.as("u").join(dfPlayList.as("p"))
+      val dfJoin = dfUser.as("u").join(dfPlayList.as("p"))
         .where(col(s"u.cpf") === col(s"p.user_id")
           and substring(col(s"p.dat_ref"), 0, 4) === lit(YEAR))
+        .groupBy(
+          col("u.name").as("name"),
+          col("u.cpf").as("cpf"),
+          col("p.style").as("style"),
+          substring(col("p.dat_ref"), 0, 4).as("year_month"))
+        .agg(sum(col("p.number_music")).as("number_all_music_by_style"),
+          count(col("p.number_music")).as("number_playlist_used_by_style"))
+
+      val dfPerfil = dfJoin.withColumn("perfil", when(col("number_all_music_by_style")
+        .between(1, 30), lit("Nesse estilo você é do tipo que se conecta com as músicas que você escuta"))
+        .when(col("number_all_music_by_style") > 31, lit("Nesse estilo você é do tipo, quanto mais variedade melhor")).otherwise("----"))
         .select(
-          col("u.name"),
-          col("u.cpf"),
-          col("p.list"),
-          substring(col("p.dat_ref"), 0, 4).as("dat_ref_ano")
-        )
+          col("name"),
+          col("cpf"),
+          col("style"),
+          col("number_all_music_by_style"),
+          col("number_playlist_used_by_style"),
+          col("perfil"),
+          col("year_month"))
 
       println(s"building partitionName and ingestionTimeStamp ...")
       val partitionName = today.getToday()
-      val ingestionTimeStamp = today.getToday()
 
       println(s"building dfToSave ...")
-      val dfToSave = dfFinal
-        .withColumn("timestamp", lit(ingestionTimeStamp))
-        .withColumn("dat_ref", lit(partitionName))
-        .select(col("name"),
+      val dfToSave = dfPerfil
+        .withColumn("timestamp", current_date())
+        .withColumn("dat_partition", lit(partitionName))
+        .withColumn("dat_ref_format", date_format(col("timestamp"), "MM-dd-yyyy"))
+        .select(
+          col("name"),
           col("cpf"),
-          col("list"),
-          col("dat_ref_ano"),
+          col("style"),
+          col("number_all_music_by_style"),
+          col("number_playlist_used_by_style"),
+          col("perfil"),
           col("timestamp"),
-          col("dat_ref")).persist()
+          col("dat_partition"),
+          col("year_month"),
+          col("dat_ref_format")).persist()
 
 
       println(s"saving dfToSave int the $TABLE_NAME_INGESTION ...")
