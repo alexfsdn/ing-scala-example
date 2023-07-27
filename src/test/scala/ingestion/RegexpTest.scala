268c727fb76afa3f83bb91a90c82d46b750625f1
Alex <alexfsdn@gmail.com>
2023-07-27 15:44:56 -0300

    update regex

--- src/test/scala/ingestion/RegexpTest.scala
+++ src/test/scala/ingestion/RegexpTest.scala
@@ -2,16 +2,19 @@ package ingestion
 
 import org.junit.Test
 import ingestion.base.services.SparkSessionServices
+import org.apache.spark.sql.SQLContext
 
 class RegexpTest {
+
+  private var hiveContext: SQLContext = null
 
   @Test
   def test(): Unit = {
     val spark = SparkSessionServices.devLocalEnableHiveSupport
 
-    val hiveContext = spark.sqlContext
+    hiveContext = spark.sqlContext
 
-    hiveContext.sql("DROP DATABASE IF EXISTS testDB CASCADE")
+    cleanup()
 
     hiveContext.sql("CREATE DATABASE IF NOT EXISTS testDB")
     hiveContext.sql("USE testDB")
@@ -26,7 +29,6 @@ class RegexpTest {
     hiveContext.sql("INSERT INTO TABLE testDB.testTable SELECT '555.555.555-55', '(44)0000000000000098232-1234'")
     hiveContext.sql("INSERT INTO TABLE testDB.testTable SELECT '666.666.666-66', '0000000000000098232-1234'")
 
-    //hiveContext.sql("select cpf, regexp_replace(cpf, '[^0-9]', '') as CPF_TRATADO, telefone, regexp_replace(telefone, '[^0-9]', '') as TELEFONE_TRATADO from testDB.testTable").show(10, false)
     hiveContext.sql("select " +
       "cpf, " +
       "regexp_replace(cpf, '[^0-9]', '') as CPF_TRATADO, telefone, " +
@@ -34,13 +36,17 @@ class RegexpTest {
       " WHEN length(regexp_replace(telefone, '[^0-9]', '')) > 11 THEN " +
       "  concat(" +
       "  regexp_replace(substr(regexp_replace(telefone, '[^0-9]', ''), 1, 2), '^0+', ''), " +
-      "  regexp_replace(substr(regexp_replace(telefone, '[^0-9]', ''), 3, length(telefone) - 2), '^0+', '')" +
+      "  regexp_replace(substr(regexp_replace(telefone, '[^0-9]', ''), 3, length(telefone)), '^0+', '')" +
       ") " +
       "ELSE " +
       "  regexp_replace(regexp_replace(telefone, '[^0-9]', ''), '^0+', '')" +
       "END AS TELEFONE_TRATADO " +
       "from testDB.testTable").show(10, false)
 
+    cleanup()
+  }
+
+  def cleanup(): Unit = {
     hiveContext.sql("DROP DATABASE IF EXISTS testDB CASCADE")
   }
 }
